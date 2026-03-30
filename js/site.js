9dca46ee0257cd397e93eb5805ce3f478d1d7142
Brad Wyatt <bradjr@gmail.com>
2026-03-30 03:00:29 -0700

    Fix loader at top of page

--- js/site.js
+++ js/site.js
@@ -197,6 +197,7 @@
         lightboxVideo.hidden = false;
         lightboxVideo.setAttribute("aria-label", altText || "Project video");
         lightboxVideo.load();
+        lightboxVideo.play().catch(() => {});
       } else {
         lightboxVideo.hidden = true;
         lightboxImage.hidden = false;
